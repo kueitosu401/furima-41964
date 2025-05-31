a052f652bab4cf127eaccfd1bc77add196af34a2
kueitosu401 <tisikizinnrey@gmail.com>
2025-05-31 19:51:52 +0900

    0531 db migrete

--- db/migrate/20250531090252_devise_create_users.rb
+++ db/migrate/20250531090252_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, unique: true
-      t.string :encrypted_password, null: false
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
       t.string :nickname,           null: false
       t.string :last_name,          null: false
       t.string :first_name_kana,    null: false
