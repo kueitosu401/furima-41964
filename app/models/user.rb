f70d152f71d2805b4ffd112d8be4c54aa05868bf
kueitosu401 <tisikizinnrey@gmail.com>
2025-06-01 09:34:01 +0900

    バリデーション　ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :email, presence: true
+         validates :encrypted_password, presence: true
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :date_of_birth, presence: true
+
+         has_many :items
+         has_many :orders
 end
