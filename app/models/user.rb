a5c2c731c70a4fdc1de844cb265c7ec7b161b202
kueitosu401 <tisikizinnrey@gmail.com>
2025-06-08 15:22:42 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }
   validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }, presence: true
   validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }, presence: true
   validates :date_of_birth, presence: true
