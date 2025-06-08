81857a06e54c1a4bdd8e2cdaf05b9498ce89c35f
kueitosu401 <tisikizinnrey@gmail.com>
2025-06-08 09:09:28 +0900

    修正0608

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }, presence: true
   validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }, presence: true
   validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }, presence: true
   validates :date_of_birth, presence: true
   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,7 @@ FactoryBot.define do
     password_confirmation { password }
     first_name            { Faker::Name.first_name }
     last_name             { Faker::Name.last_name }
+    first_name_kana       { Faker::Name.first_name_kana }
+    last_name_kana        { Faker::Name.last_name_kana }
   end
 end
