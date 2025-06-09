5a3d5f793e217523e32bbfb729501ea2441731b2
kueitosu401 <tisikizinnrey@gmail.com>
2025-06-09 18:02:10 +0900

    Active Storageの導入

---
+++ db/migrate/20250609072639_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+
+t.string           :product_name,             null: false
+t.text             :product_description,      null: false
+t.references       :category_id,              null: false
+t.integer          :shipping_costs_id,        null: false
+t.integer          :region_of_origin_id,      null: false
+t.integer          :condition_id,             null: false
+t.integer          :prefecture_id,            null: false
+t.references       :user,                     null: false, foreign_key: true
+t.integer          :sales_price,              null: false
+
+      t.timestamps
+    end
+  end
+end
