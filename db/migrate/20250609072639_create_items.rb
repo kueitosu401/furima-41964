3369de307a026a36e784ce9b508d229e95585f6f
kueitosu401 <tisikizinnrey@gmail.com>
2025-06-13 17:48:20 +0900

    修正

--- db/migrate/20250609072639_create_items.rb
+++ db/migrate/20250609072639_create_items.rb
@@ -11,6 +11,7 @@ t.integer          :condition_id,             null: false
 t.integer          :prefecture_id,            null: false
 t.references       :user,                     null: false, foreign_key: true
 t.integer          :sales_price,              null: false
+t.timestamps
 
       t.timestamps
     end
