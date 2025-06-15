170a3a1de5cf08bbeb79045f30ed7d741800a09b
kueitosu401 <tisikizinnrey@gmail.com>
2025-06-15 21:22:31 +0900

    プルダウン　番号修正

--- app/models/Shipping_costs.rb
+++ app/models/Shipping_costs.rb
@@ -1,7 +1,7 @@
 class ShippingCosts < ActiveHash::Base
   self.data = [
-    { id: 1, charge: '着払い(購入者負担)' },
-    { id: 2, charge: '送料込み(出品者負担)' }
+    { id: 0, charge: '着払い(購入者負担)' },
+    { id: 1, charge: '送料込み(出品者負担)' }
   ]
 
   include ActiveHash::Associations
