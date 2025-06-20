817f0b30005a980df97340ed5dbbacf69605406b
kueitosu401 <tisikizinnrey@gmail.com>
2025-06-20 18:18:49 +0900

    item購入　コントローラー処理　indexまで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,29 @@
 class OrdersController < ApplicationController
 
   def index
-
+    @order = Order.new
+    @item = Item.find(params[:item_id])
   end  
 
   def new
   @item = Item.find(params[:item_id])
   @order = Order.new
   end
-end  
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to some_path, notice: 
+    else
+      render :new
+    end
+  end  
+
+
+
+ private
+
+  def order_params
+   params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
+  end
+end
